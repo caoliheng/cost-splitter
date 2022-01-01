cf416cb39bf88949b41d11550dab03a9d3342b33
Liheng Cao <lihengcao@nyu.edu>
2022-01-01 17:34:26 -0600

    continue

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-function main(){
-    console.log("asdf");
+function main(){ // generic main
+    console.log("main()");
 }
 
 main();
 
-function onButton(){
+// window.onbeforeunload = function() { return "Data will be lost if you leave the page, are you sure?"; };// gives warning before reloading or leaving site
+
+function onButton(){ // button function
     console.log("button pressed!");
+    
+    var table = document.getElementById("table"); // get the table
+
+    var new_row = table.insertRow(-1); // append row
+    
+    var new_item = new_row.insertCell(-1); // append cell
+    var new_cost = new_row.insertCell(-1); // append cell
+    var new_split = new_row.insertCell(-1);
+
+    new_item.innerHTML = Math.floor(Math.random() * 10); // give random data
+    new_item.contentEditable = true;
+
+    new_cost.innerHTML = Math.floor(Math.random() * 10); // give random data
+    new_cost.contentEditable = true;
+
+    new_split.innerHTML = Math.floor(Math.random() * 10); // give random data
+    new_split.contentEditable = true;
 }
