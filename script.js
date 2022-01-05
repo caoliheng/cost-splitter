c9f515e7022d7f47cd4ace0988926c428fa6c7b9
Liheng Cao <lihengcao@nyu.edu>
2022-01-05 15:54:26 -0600

    added checkboxes

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 // gives warning before reloading or leaving site
 // window.onbeforeunload = function() { return "Data will be lost if you leave the page, are you sure?"; }; 
 
-function onButton(){ // button function
-    var table = document.getElementById("table"); // get the table
+function addItem(){ // button function
+    let table = document.getElementById("table"); // get the table
 
-    var new_row = table.insertRow(-1); // append row
+    let new_row = table.insertRow(-1); // append row
     
-    var new_item = new_row.insertCell(-1); // append cell
-    var new_cost = new_row.insertCell(-1); // append cell
-    var new_split = new_row.insertCell(-1); // append cell
+    let new_item = new_row.insertCell(-1); // append cell
+    let new_cost = new_row.insertCell(-1); // append cell
+    let new_split = new_row.insertCell(-1); // append cell
 
     new_item.innerHTML = Math.floor(Math.random() * 10); // give random data
     new_item.contentEditable = true; // make this editable
@@ -16,21 +16,27 @@ function onButton(){ // button function
     new_cost.innerHTML = Math.floor(Math.random() * 10); // give random data
     new_cost.contentEditable = true; // make this editable
     new_cost.className = "cost"; // give class name of cost
-    new_cost.addEventListener("focusout", unfocus); // give function to update total price
+    new_cost.addEventListener("focusout", costUnfocus); // give function to update total price
 
     new_split.innerHTML = Math.floor(Math.random() * 10); // give random data
     new_split.contentEditable = true;
 }
 
-function unfocus(){
-    var sum = 0; // sum of 
-    var numbers = document.getElementsByClassName("cost");
+function costUnfocus(){
+    let sum = 0; // sum of 
+    let numbers = document.getElementsByClassName("cost");
 
-    for (var i = numbers.length - 1; i != -1; --i){ // reverse is faster because .length only used once ...
-        var f = parseFloat(numbers[i].textContent)
-        if (f != NaN){
+    for (let i = numbers.length - 1; i != -1; --i){ // reverse is faster because .length only used once ...
+        let f = parseFloat(numbers[i].textContent) // convert to float
+        if (!isNaN(f)){ // ignore if NaN
             sum += f;
         }
     }
-    document.getElementById("display").innerHTML = sum.toFixed(2);
+    document.getElementById("display").textContent = sum.toFixed(2); // display with precision 2
+
+    console.log(document.getElementById("0,0").checked);
+}
+
+function addPerson(){
+    let table = document.getElementById("table");
 }
