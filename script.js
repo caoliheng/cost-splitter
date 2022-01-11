74398164dfc7b005715a05652e20ba5b38196af1
Liheng Cao <lihengcao@nyu.edu>
2022-01-10 20:39:03 -0600

    dynamically adds checkboxes

--- script.js
+++ script.js
@@ -1,25 +1,47 @@
 // gives warning before reloading or leaving site
 // window.onbeforeunload = function() { return "Data will be lost if you leave the page, are you sure?"; }; 
 
+// let personDict = {};
+
+// keeps track of the names and number of people
+let people = ["Person A", "Person B", "Person C"];
+
+
 function addItem(){ // button function
     let table = document.getElementById("table"); // get the table
 
     let new_row = table.insertRow(-1); // append row
     
+    // First column is Item Name
     let new_item = new_row.insertCell(-1); // append cell
-    let new_cost = new_row.insertCell(-1); // append cell
-    let new_split = new_row.insertCell(-1); // append cell
-
     new_item.innerHTML = Math.floor(Math.random() * 10); // give random data
     new_item.contentEditable = true; // make this editable
 
+    // Second Column is Item Cost
+    let new_cost = new_row.insertCell(-1); // append cell
     new_cost.innerHTML = Math.floor(Math.random() * 10); // give random data
     new_cost.contentEditable = true; // make this editable
     new_cost.className = "cost"; // give class name of cost
     new_cost.addEventListener("focusout", costUnfocus); // give function to update total price
 
-    new_split.innerHTML = Math.floor(Math.random() * 10); // give random data
-    new_split.contentEditable = true;
+    // Third column is the spacer for split
+    let new_spacer = new_row.insertCell(-1); // append cell
+    new_spacer.className = "spacer";
+
+    for (let i = 0; i < people.length; ++i){
+        // create <td/>
+        let new_person = new_row.insertCell(-1);
+
+        // create <input/> 
+        var input = document.createElement("input");
+        // make checkbox
+        input.setAttribute('type', 'checkbox');
+
+        // put <input/? inside <td/?
+        new_person.appendChild(input);
+    }
+
+
 }
 
 function costUnfocus(){
@@ -34,9 +56,11 @@ function costUnfocus(){
     }
     document.getElementById("display").textContent = sum.toFixed(2); // display with precision 2
 
-    console.log(document.getElementById("0,0").checked);
+    // console.log(document.getElementById("0,0").checked);
 }
 
 function addPerson(){
     let table = document.getElementById("table");
+    let header = table.rows[0];
+    console.log(table.rows[0]);
 }
